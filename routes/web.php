4145800823c818e2eb54fb99b32055b282cbb517
mana0001 <robin0515.m@gmail.com>
2023-12-07 01:11:36 +0900

    モデル・コントローラー・ビュー

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+Route::resource('tasks', TaskController::class);
 });
